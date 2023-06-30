b0342fe5de553b8f341075a57451b022b73408a4
Iason Andronis <iasona98@gmail.com>
2023-06-30 17:13:20 +0200

    reorganized scripts

--- scripts/autorun.py
+++ scripts/autorun.py
@@ -9,7 +9,7 @@ import subprocess
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Gromacs run automation script")
-    root = pathlib.Path(__file__).parent.resolve()
+    root = pathlib.Path(__file__).parent.parent
     mdf = pathlib.Path(root, "mdf")
     mdp = pathlib.Path(root, "mdp")
     box = pathlib.Path(root, "box.gro")
@@ -33,8 +33,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "-p", "--partition", type=str, default="cops", help="Partition to run on"
     )
-    parser.add_argument("--temp", type=int, default=300, help="Reference temperature")
-    parser.add_argument("--pressure", type=int, default=1, help="Reference pressure")
+    parser.add_argument("--temp", type=int, required=True, help="Reference temperature")
+    parser.add_argument("--pressure", type=int, default=1, required=True, help="Reference pressure")
     parser.add_argument(
         "--equilibration-time",
         type=int,
@@ -48,6 +48,10 @@ if __name__ == "__main__":
         help="Production run time in nanoseconds",
     )
     parser.add_argument("--launch", action="store_true", help="Launch run")
+    parser.add_argument("--gpu", action="store_true", help="Use GPU")
+    parser.add_argument(
+        "--solution-name", type=str, default="water", help="Solution name"
+    )
 
     args = parser.parse_args()
     print("OUTPUT: ", args.output_path)
@@ -58,7 +62,7 @@ if __name__ == "__main__":
     if args.launch:
         print("Running Gromacs...")
 
-    run_prefix = f"water_{args.temp}K_{args.pressure}bar"
+    run_prefix = f"{args.solution_name}_{args.temp}K_{args.pressure}bar"
     current_time = datetime.now().strftime("%d-%m-%Y_%H:%M")
     output_dir = pathlib.Path(args.output_path, f"{run_prefix}_{current_time}")
     log = pathlib.Path(output_dir, "run.log")
@@ -120,7 +124,7 @@ if __name__ == "__main__":
     with open(md_mdp, "r") as f:
         nsteps = re.sub(
             nsteps_regex,
-            f"nsteps = {md_nsteps}  ; 0.002 * {npt_nsteps} = {args.production_time} ns",
+            f"nsteps = {md_nsteps}  ; 0.002 * {md_nsteps} = {args.production_time} ns",
             f.read(),
             0,
             flags=re.MULTILINE,
@@ -130,11 +134,29 @@ if __name__ == "__main__":
 
     # Launch jobs
     if args.launch:
-        em_run_cmd = f"sbatch -p {args.partition} --output={log} --open-mode=append --parsable em_run.sbatch -o {output_dir} -f {em_mdp} -p {args.topology} -c {args.box}"
-        em_run = subprocess.run(em_run_cmd.split(), stdout=subprocess.PIPE)
-        nvt_run_cmd = f"sbatch -p {args.partition} --output={log} --open-mode=append --dependency=afterok:{em_run.stdout.decode('utf-8')} --parsable nvt_run.sbatch -o {output_dir} -f {nvt_mdp} -p {args.topology}"
-        nvt_run = subprocess.run(nvt_run_cmd.split(), stdout=subprocess.PIPE)
-        npt_run_cmd = f"sbatch -p {args.partition} --output={log} --open-mode=append --dependency=afterok:{nvt_run.stdout.decode('utf-8')} --parsable npt_run.sbatch -o {output_dir} -f {npt_mdp} -p {args.topology}"
-        npt_run = subprocess.run(npt_run_cmd.split(), stdout=subprocess.PIPE)
-        md_run_cmd = f"sbatch -p {args.partition} --output={log} --open-mode=append --dependency=afterok:{npt_run.stdout.decode('utf-8')} --parsable md_run.sbatch -o {output_dir} -f {md_mdp} -p {args.topology}"
-        md_run = subprocess.run(md_run_cmd.split(), stdout=subprocess.PIPE)
+        scripts = pathlib.Path(__file__).parent
+        if not args.gpu:
+            em_script = pathlib.Path(scripts, "em_run.sbatch")
+            em_run_cmd = f"sbatch -p {args.partition} --output={log} --open-mode=append --parsable {em_script} -o {output_dir} -f {em_mdp} -p {args.topology} -c {args.box}"
+            em_run = subprocess.run(em_run_cmd.split(), stdout=subprocess.PIPE)
+            nvt_script = pathlib.Path(scripts, "nvt_run.sbatch")
+            nvt_run_cmd = f"sbatch -p {args.partition} --output={log} --open-mode=append --dependency=afterok:{em_run.stdout.decode('utf-8')} --parsable {nvt_script} -o {output_dir} -f {nvt_mdp} -p {args.topology}"
+            nvt_run = subprocess.run(nvt_run_cmd.split(), stdout=subprocess.PIPE)
+            npt_script = pathlib.Path(scripts, "npt_run.sbatch")
+            npt_run_cmd = f"sbatch -p {args.partition} --output={log} --open-mode=append --dependency=afterok:{nvt_run.stdout.decode('utf-8')} --parsable {npt_script} -o {output_dir} -f {npt_mdp} -p {args.topology}"
+            npt_run = subprocess.run(npt_run_cmd.split(), stdout=subprocess.PIPE)
+            md_script = pathlib.Path(scripts, "md_run.sbatch")
+            md_run_cmd = f"sbatch -p {args.partition} --output={log} --open-mode=append --dependency=afterok:{npt_run.stdout.decode('utf-8')} --parsable {md_script} -o {output_dir} -f {md_mdp} -p {args.topology}"
+            md_run = subprocess.run(md_run_cmd.split(), stdout=subprocess.PIPE)
+        else:
+            em_run_cmd = f"sbatch -p ampere --output={log} --open-mode=append --parsable em_run.sbatch -o {output_dir} -f {em_mdp} -p {args.topology} -c {args.box}"
+            em_run = subprocess.run(em_run_cmd.split(), stdout=subprocess.PIPE)
+            nvt_script = pathlib.Path(scripts, "nvt_run_GPU.sbatch")
+            nvt_run_cmd = f"sbatch --output={log} --open-mode=append --dependency=afterok:{em_run.stdout.decode('utf-8')} --parsable {nvt_script} -o {output_dir} -f {nvt_mdp} -p {args.topology}"
+            nvt_run = subprocess.run(nvt_run_cmd.split(), stdout=subprocess.PIPE)
+            npt_script = pathlib.Path(scripts, "npt_run_GPU.sbatch")
+            npt_run_cmd = f"sbatch --output={log} --open-mode=append --dependency=afterok:{nvt_run.stdout.decode('utf-8')} --parsable {npt_script} -o {output_dir} -f {npt_mdp} -p {args.topology}"
+            npt_run = subprocess.run(npt_run_cmd.split(), stdout=subprocess.PIPE)
+            md_script = pathlib.Path(scripts, "md_run_GPU.sbatch")
+            md_run_cmd = f"sbatch --output={log} --open-mode=append --dependency=afterok:{npt_run.stdout.decode('utf-8')} --parsable {md_script} -o {output_dir} -f {md_mdp} -p {args.topology}"
+            md_run = subprocess.run(md_run_cmd.split(), stdout=subprocess.PIPE)
